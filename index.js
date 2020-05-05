4e7c845a8c93a36c30d293f2fbe280f10c087204
evan <aspaasevan@gmail.com>
2020-05-05 14:42:11 -0700

    setting up blogRouter

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+const blogRouter = require('./blogs/blogs_router');
+
+server.get('/', (req, res) => {
+  res.json("Server is running.")
+})
+
+// server.use('/blogs', blogRouter);
+
+server.listen(8000, () => console.log('API running on port 8000'));
