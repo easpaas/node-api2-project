c5f8df51b847d27f284e9fa3896d1594a8792d5c
evan <aspaasevan@gmail.com>
2020-05-06 11:48:24 -0700

    App renders blog posts

--- api2-project/src/App.js
+++ api2-project/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/posts")
+      .then(response => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Read all about it</h1>
+      <div className="Blog">
+        {
+          data.map(post => {
+            <div key={post.id} className="Post">
+              <h2>{data.title}</h2>
+              <p>{data.contents}</p>
+            </div>;
+          })
+        }
+      </div>
     </div>
   );
 }
