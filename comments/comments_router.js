991094d3da424e74efdc9987c5d57e6fde4f0456
evan <aspaasevan@gmail.com>
2020-05-05 22:21:53 -0700

    quick save

--- comments/comments_router.js
+++ comments/comments_router.js
@@ -19,4 +19,29 @@ router.get("/:id", (req, res) => {
     });
 })
 
+router.get('/:id', (req, res) => {
+  db.findCommentById(id)
+    .then(comment => {
+      comment ?
+        res.status(200).json(comment)
+      :
+        res.status(404).json({ message: 'Comment could not be found'})
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'Trouble connecting to server'});
+    })
+})
+
+router.post('/', (req, res) => {
+  const comment = req.body;
+  db.insertComment(comment)
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ errorMessage: 'Trouble connecting to server'});
+    });
+});
+
 module.exports = router;
