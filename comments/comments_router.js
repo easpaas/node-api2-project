f94e575b717d61316a4883e7c5111458783a104f
evan <aspaasevan@gmail.com>
2020-05-06 08:50:24 -0700

    returns all comments by post id

--- comments/comments_router.js
+++ comments/comments_router.js
@@ -1,47 +1,47 @@
-const express = require("express");
+// const express = require("express");
 
-const db = require("../data/db");
+// const db = require("../data/db");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  db.findPostComments(id)
-    .then(comments => {
-      comments ?
-        res.status(200).json(comments) 
-      :
-        res.status(404).json({ message: 'Post could not be found'});
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(500).json({ errorMessage: 'Trouble connecting to server'})
-    });
-})
+// router.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   db.findPostComments(id)
+//     .then(comments => {
+//       comments ?
+//         res.status(200).json(comments) 
+//       :
+//         res.status(404).json({ message: 'Post could not be found'});
+//     })
+//     .catch(error => {
+//       console.log(error)
+//       res.status(500).json({ errorMessage: 'Trouble connecting to server'})
+//     });
+// })
 
-router.get('/:id', (req, res) => {
-  db.findCommentById(id)
-    .then(comment => {
-      comment ?
-        res.status(200).json(comment)
-      :
-        res.status(404).json({ message: 'Comment could not be found'})
-    })
-    .catch(error => {
-      res.status(500).json({ errorMessage: 'Trouble connecting to server'});
-    })
-})
+// router.get('/:id', (req, res) => {
+//   db.findCommentById(id)
+//     .then(comment => {
+//       comment ?
+//         res.status(200).json(comment)
+//       :
+//         res.status(404).json({ message: 'Comment could not be found'})
+//     })
+//     .catch(error => {
+//       res.status(500).json({ errorMessage: 'Trouble connecting to server'});
+//     })
+// })
 
-router.post('/', (req, res) => {
-  const comment = req.body;
-  db.insertComment(comment)
-    .then(post => {
-      res.status(201).json(post)
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(500).json({ errorMessage: 'Trouble connecting to server'});
-    });
-});
+// router.post('/', (req, res) => {
+//   const comment = req.body;
+//   db.insertComment(comment)
+//     .then(post => {
+//       res.status(201).json(post)
+//     })
+//     .catch(error => {
+//       console.log(error)
+//       res.status(500).json({ errorMessage: 'Trouble connecting to server'});
+//     });
+// });
 
-module.exports = router;
+// module.exports = router;
--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const server = express();
 server.use(express.json());
 
 const postRouter = require('./posts/posts_router');
-const commentRouter = require('./comments/comments_router');
+// const commentRouter = require('./comments/comments_router');
 
 server.get('/', (req, res) => {
   res.json("Server is running.")
 })
 
 server.use('/api/posts', postRouter);
-server.use('/api/comments', commentRouter);
 
 server.listen(8000, () => console.log('API running on port 8000'));
